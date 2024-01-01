f1b8a4420f4e83df7588336208919860bdfea825
Sahith Akula <akulasahith268@gmail.com>
2024-01-01 14:03:05 +0530

    Trees were updated

---
+++ BinarySearchTree.java
@@ -0,0 +1,126 @@
+package DSA;
+import java.util.*;
+class BST{
+    private Node root;
+    private class Node{
+     private int height;
+     private int value;
+      Node right;
+      Node left;
+    public   Node(int value){
+    this.value = value;
+     } 
+    }
+    public void insertRoot(Scanner sc){
+        System.out.println("Enter root node value:-");
+        int value = sc.nextInt();
+        root = new Node(value);
+    }
+    public void populate(int[] nums){
+        for(int i = 0;i<nums.length;i++){
+            insert(nums[i]);
+        }
+    }
+    public void  inOrderTraversal(){
+         inOrderTraversal(root);
+    }
+    private void inOrderTraversal(Node node){
+        if(node==null){
+            return;
+        }
+        inOrderTraversal(node.left);
+        System.out.println("Node value = " + node.value);
+        inOrderTraversal(node.right);
+    }
+    public void PreOrderTraversal(){
+        PreOrderTraversal(root);
+    }
+    private void PreOrderTraversal(Node node){
+        if(node==null){
+            return;
+        }
+        System.out.println("Node value = " + node.value);
+        PreOrderTraversal(node.left);
+        PreOrderTraversal(node.right);
+    }
+    public void insert(int value){
+       root =  insert(value, root);
+    }
+    private Node insert(int value,Node node){
+        if(node==null){
+           Node temp = new Node(value);
+           return temp;
+        }
+
+        if(value<node.value){
+            node.left = insert(value, node.left);
+        }
+        if(value>node.value){
+            node.right = insert(value,node.right);
+        }
+
+        node.height = Math.max(Height(node.left),Height(node.right));
+        return node;
+    }
+    public boolean isbalanced(){
+      return isbalanced(root);
+    }
+    private boolean isbalanced(Node node){
+      if(node==null){
+        return true;
+      }
+
+
+      return Math.abs(Height(node.left)-Height(node.right))<=1&&isbalanced(node.left)&&isbalanced(node.right);
+    }
+     public int Height(Node node) {
+        if(node==null){
+            return -1;
+        }else{
+            return node.height;
+        }
+    }
+    public void DisplayTree(){
+        DisplayTree(root,"Root Node:-");
+    }
+    public void DisplayTree(Node node,String details){
+        if(node==null){
+            return;
+        }
+        System.out.println(details + " " + node.value);
+        DisplayTree(node.left,"Left child of"+ " " + node.value);
+        DisplayTree(node.right,"Right child of"+ " " + " " + node.value);
+    }
+    public void postOrderTraversal(){
+      postOrderTraversal(root);
+    }
+    public void postOrderTraversal(Node node){
+      if(node==null){
+        return;
+      }
+    postOrderTraversal(node.left);
+    postOrderTraversal(node.right);
+    System.out.println("Node value = " + node.value);
+    }
+    
+}
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BST tree = new BST();
+        System.out.println("Enter how many values to insert in tree:-");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0;i<n;i++){
+            nums[i] = sc.nextInt();
+        }
+        tree.populate(nums);
+        tree.DisplayTree();
+        System.out.println("Pre order traversal:-");
+        tree.PreOrderTraversal();
+        System.out.println("In order traversal:-");
+        tree.inOrderTraversal();
+        System.out.println("Post order traversal");
+        tree.postOrderTraversal();
+    }
+}
--- Trees.java
+++ Trees.java
@@ -1,66 +1,66 @@
 package DSA;
 import java.util.*;
 class BinaryTree{
-    public BinaryTree(){
-
+ private  Node root; 
+    class Node{
+        int value;
+        Node right;
+        Node left;
+        public Node(int value){
+         this.value = value;
+        }
     }
-   private class Node{
-    int value;
-    Node right;
-    Node left;
-    public Node(int value){
-        this.value = value;
+    public void DisplayTree(){
+        DisplayTree(root,0);
     }
-   }
-   private Node root;
-    public void displaytree(){
-        System.out.println(root.value);
-        Node temp = root;
-        helper(root);
-    }
-    public void helper(Node node){
-        if(node.left==null||node.right==null){
+    public void DisplayTree(Node node,int level){
+        if(node==null){
             return;
         }
-        System.out.println(node.left.value);
-        System.out.println(node.right.value);
-        node = node.left;
-        helper(node);
-        node = node.right;
-        helper(node);
+        DisplayTree(node.right, level + 1);
+        if(level!=0){
+        for(int i=0;i<level-1;i++){
+           System.out.print("|  ");
+        }
+        System.out.println("|-------->" + node.value);
+        }else{
+            System.out.println(node.value);
+        }
+        DisplayTree(node.left, level + 1);
     }
-   public void populate(Scanner sc){
-    System.out.print("Enter the root value of the tree:");
-    int value = sc.nextInt();
-     root = new Node(value);
-     populate(sc,root);
-   }
-
- public void populate(Scanner sc, Node node) {
-    System.out.println("Enter if you want to enter on the left of " + node.value);
-    boolean left = sc.nextBoolean();
-    if(left){
-        System.out.println("Enter the value");
+    public void populate(Scanner sc){
+      System.out.print("Enter root node value: ");
+      int val = sc.nextInt();
+      root = new Node(val);
+      populate(sc,root);
+    }
+    public void populate(Scanner sc,Node node){
+       System.out.println("Want to enter left of" +" "+node.value);
+       System.out.println("Enter true or false");
+       boolean left = sc.nextBoolean();
+       if(left){
+        System.out.println("Enter value:");
         int value = sc.nextInt();
         node.left = new Node(value);
         populate(sc,node.left);
-    }
-    System.out.println("Enter if you want to enter on the right of " + node.value);
-    boolean right = sc.nextBoolean();
-    if(right){
-        System.out.println("Enter the value");
+       }
+       System.out.println("Want to enter right of" + node.value);
+       System.out.println("Enter true or false");
+       boolean right= sc.nextBoolean();
+       if(right){
+        System.out.println("Enter value:");
         int value = sc.nextInt();
         node.right = new Node(value);
         populate(sc,node.right);
+       }
+
     }
-   // System.out.println("Populating process is ended");
 }
+public class Trees{
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    BinaryTree tree = new BinaryTree();
+    tree.populate(sc);
+    tree.DisplayTree();
 }
-class Trees{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BinaryTree tree = new BinaryTree();
-        tree.populate(sc);
-        tree.displaytree();
-    }
 }
